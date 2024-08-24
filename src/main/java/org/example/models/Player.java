0456998163c1e4702dacd5d7392e4be33c0ff7d7
Shubham Patel <sp4227abc@gmail.com>
2024-08-25 03:46:28 +0530

    First Commit

---
+++ src/main/java/org/example/models/Player.java
@@ -0,0 +1,12 @@
+package org.example.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+    private int id;
+    private String name;
+    private char symbol;
+}
